a2db81184e4f5136834f916771bebb4b5383926e
Vinicius Hespanha Souza <mail@vhespanha.dev>
2026-06-22 21:23:13 -0300

    dynamically import Hono app from entry point file

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
 import { parseArgs } from "@std/cli";
 import { printf } from "@std/fmt/printf";
+import { Hono } from "hono";
 
-function main(): void {
+async function getHonoApp(entryPointPath: string): Promise<Hono> {
+  const { default: app } = await import(entryPointPath);
+
+  if (!(app instanceof Hono)) {
+    throw new Error("Entry point must default-export a `Hono` instance.");
+  }
+
+  return app;
+}
+
+async function main(): Promise<void> {
   const { _: [entryPointPath] } = parseArgs(Deno.args);
 
   if (entryPointPath === undefined) {
@@ -9,8 +20,19 @@ function main(): void {
     Deno.exit(1);
   }
 
+  let app;
+
+  try {
+    app = await getHonoApp(String(entryPointPath));
+  } catch (error) {
+    console.error(`Failed to load app from ${entryPointPath}: `, error);
+    throw error;
+  }
+
   printf("Hello, Pangea!\n");
-  printf("Entry point is: %s.\n", entryPointPath);
+  printf("Entry point app is valid!\n");
+
+  Deno.serve(app.fetch);
 }
 
 if (import.meta.main) main();
