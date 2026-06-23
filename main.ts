2c06e76c67b904be862ea6df24a6412c9540de12
Vinicius Hespanha Souza <mail@vhespanha.dev>
2026-06-22 21:19:47 -0300

    parse entry point arg with @std/cli

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
+import { parseArgs } from "@std/cli";
 import { printf } from "@std/fmt/printf";
 
 function main(): void {
+  const { _: [entryPointPath] } = parseArgs(Deno.args);
+
+  if (entryPointPath === undefined) {
+    console.error("Usage: pangea <entrypoint>");
+    Deno.exit(1);
+  }
+
   printf("Hello, Pangea!\n");
+  printf("Entry point is: %s.\n", entryPointPath);
 }
 
 if (import.meta.main) main();
